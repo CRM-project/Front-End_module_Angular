6f0978231260261a822d2b6a5116a07aa53f8aeb
milomory <milomory@gmail.com>
2015-11-28 01:02:53 +0300

    html5mod on

--- sources/webapp/scripts/app.js
+++ sources/webapp/scripts/app.js
@@ -1,1 +1,21 @@
-var crm = angular.module('crm', []);
+var crm = angular.module('crm', ['ngRoute', 'ngResource']);
+
+crm.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        }).hashPrefix('!');
+
+        $routeProvider.
+            when('/', {
+               templateUrl: 'tpl/test.html'
+            }).
+            when('/test', {
+                templateUrl: 'tpl/test.html'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
